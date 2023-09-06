6612e00314b9ac87da693f1a616e4e809b0ae748
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2023-09-06 19:45:37 +0330

    complete

--- Back End Development and APIs/URL Shortener Microservice/index.js
+++ Back End Development and APIs/URL Shortener Microservice/index.js
@@ -2,23 +2,50 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors(), express.json(), express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+const urlDatabase = {}
+
+app.post("/api/shorturl", (req, res) => {
+  const url = new URL(req.body.url)
+  const options = {
+    all: true,
+  };
+  dns.lookup(url.hostname, options, (err, addresses) => {
+    if (err) {
+      res.json({ error: "invalid URL" })
+    } else {
+      const shortUrl = addresses[0].address.split(".").join("")
+      urlDatabase[shortUrl] = req.body.url
+      res.json({ original_url: req.body.url, short_url: shortUrl })
+    }
+  })
+})
+
+app.get("/api/shorturl/:shortUrl", (req, res) => {
+  if (urlDatabase[req.params.shortUrl]) {
+    res.redirect(urlDatabase[req.params.shortUrl])
+  } else {
+    res.json({ error: 'invalid url' })
+  }
+})
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
