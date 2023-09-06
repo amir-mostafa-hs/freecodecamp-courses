620f6a6ce2c0c95d46b6be1e2762bda5251c56b3
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2023-09-06 21:42:13 +0330

    complete exercises

--- Back End Development and APIs/Exercise Tracker/index.js
+++ Back End Development and APIs/Exercise Tracker/index.js
@@ -44,6 +44,65 @@ const createUserController = (req, res) => {
 
 app.route("/api/users").get(findUserController).post(createUserController);
 
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  const user_id = req.params._id;
+  const user = await User.findById(user_id);
+  if (!user) return res.json({ error: "User of the given 'id' doesn't exists" });
+
+  const { description, duration, date } = req.body;
+  if (!description) return res.json({ error: "Please provide Exercise description to proceed" });
+  if (!duration) return res.json({ error: "Please provide Exercise duration to proceed" });
+
+  const exercise = await new Exercise({
+    user_id: user._id,
+    description: description,
+    duration: duration,
+    date: date ? new Date(date) : new Date(),
+  }).save();
+
+  res.json({
+    _id: user.id,
+    username: user.username,
+    description: exercise.description,
+    duration: exercise.duration,
+    date: new Date(exercise.date).toDateString(),
+  });
+});
+
+app.get("/api/users/:_id/logs", async (req, res) => {
+  const user_id = req.params._id;
+  const { from, to, limit } = req.query;
+
+  const user = await User.findById(user_id);
+  if (!user) return res.json({ error: "User of the given 'id' doesn't exists" });
+
+  const searchObj = { user_id: user_id };
+  const data = {};
+  if (from) {
+    searchObj.date.$gte = new Date(from);
+    data.from = from;
+  }
+  if (to) {
+    searchObj.date.$lte = new Date(to);
+    data.to = to;
+  }
+
+  const exercise = await Exercise.find(searchObj).limit(+limit);
+  if (!exercise) return res.json({ error: "No exercise exists for the given 'id'" });
+  const log = exercise.map((item) => ({
+    description: item.description,
+    duration: item.duration,
+    date: item.date.toDateString(),
+  }));
+
+  data.username = user.username;
+  data.count = exercise.length;
+  data._id = exercise.user_id;
+  data.log = log;
+
+  res.json(data);
+});
+
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
